9843170ce6755696a89edd6a931f88cdd82f0bee
weiping <weiping@atlasv.com>
2024-12-25 17:25:03 +0800

    refactor: Remove AmplifyModelMerger

--- simple-appsync/src/main/java/com/atlasv/android/amplify/simpleappsync/AmplifySimpleSyncComponent.kt
+++ simple-appsync/src/main/java/com/atlasv/android/amplify/simpleappsync/AmplifySimpleSyncComponent.kt
@@ -1,29 +1,16 @@
 package com.atlasv.android.amplify.simpleappsync
 
 import android.content.Context
-import com.amplifyframework.api.aws.AppSyncGraphQLRequest
-import com.amplifyframework.api.graphql.GraphQLRequest
-import com.amplifyframework.api.graphql.GraphQLResponse
-import com.amplifyframework.api.graphql.PaginatedResult
-import com.amplifyframework.core.model.Model
 import com.amplifyframework.core.model.ModelProvider
 import com.amplifyframework.core.model.SchemaRegistry
 import com.amplifyframework.datastore.DataStoreConfiguration
-import com.amplifyframework.datastore.appsync.ModelWithMetadata
 import com.amplifyframework.datastore.storage.sqlite.CursorValueStringFactory
 import com.amplifyframework.datastore.storage.sqlite.SQLCommandFactoryFactory
 import com.amplifyframework.kotlin.core.Amplify
 import com.atlasv.android.amplify.simpleappsync.config.AmplifySimpleSyncConfig
 import com.atlasv.android.amplify.simpleappsync.ext.AmplifyExtSettings
-import com.atlasv.android.amplify.simpleappsync.ext.simpleFormat
-import com.atlasv.android.amplify.simpleappsync.request.DefaultMergeRequestFactory
-import com.atlasv.android.amplify.simpleappsync.request.MergeRequestFactory
-import com.atlasv.android.amplify.simpleappsync.response.AmplifyModelMerger
-import com.atlasv.android.amplify.simpleappsync.response.AmplifySyncResponse
 import com.atlasv.android.amplify.simpleappsync.storage.AmplifySqliteStorage
 import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
-import java.util.Date
 
 /**
  * weiping@atlasv.com
@@ -34,7 +21,6 @@ class AmplifySimpleSyncComponent(
     val dataStoreConfiguration: DataStoreConfiguration,
     val modelProvider: ModelProvider,
     val schemaRegistry: SchemaRegistry,
-    private val mergeListFactory: MergeRequestFactory,
     sqlCommandFactoryFactory: SQLCommandFactoryFactory,
     cursorValueStringFactory: CursorValueStringFactory,
     private val buildInDbMigrate: (String) -> Unit,
@@ -44,7 +30,6 @@ class AmplifySimpleSyncComponent(
     val extSettings by lazy {
         AmplifyExtSettings(appContext)
     }
-    private val mutex = Mutex()
     val storage by lazy {
         AmplifySqliteStorage(
             appContext,
@@ -58,60 +43,6 @@ class AmplifySimpleSyncComponent(
             config,
             onSqliteInitSuccess,
         )
-    }
-
-    val merger by lazy {
-        AmplifyModelMerger(storage)
-    }
-
-    suspend fun syncFromRemote(): AmplifySyncResponse? {
-        return mutex.withLock {
-            try {
-                val lastSync = extSettings.getLastSyncTimestamp()
-                val grayRelease = config.grayRelease
-                val currentTime = System.currentTimeMillis()
-                val request = mergeListFactory.create(
-                    appContext, dataStoreConfiguration, modelProvider, schemaRegistry, lastSync, grayRelease
-                )
-                val responseItemGroups = queryAllData(request)
-                val newestSyncTime = responseItemGroups.maxOfOrNull { list ->
-                    list.maxOfOrNull {
-                        it.syncMetadata.lastChangedAt?.secondsSinceEpoch ?: 0L
-                    } ?: 0L
-                }?.takeIf { it > lastSync }?.coerceAtMost(currentTime)?.also {
-                    LOG.info("newestSyncTime=$it")
-                }
-                val succeed = merger.mergeResponse(responseItemGroups)
-                if (!succeed) {
-                    LOG.info("mergeResponse failed")
-                    return null
-                }
-                extSettings.saveLastSyncTimestamp(newestSyncTime)
-                val newestUpdatedTime = newestSyncTime ?: lastSync
-                LOG.info(
-                    "syncFromRemote success, newestUpdatedTime=${Date(newestUpdatedTime).simpleFormat()}"
-                )
-                AmplifySyncResponse(newestUpdatedTime = newestUpdatedTime)
-            } catch (cause: Throwable) {
-                LOG.error("syncFromRemote error", cause)
-                null
-            }
-        }
-    }
-
-    private suspend fun queryAllData(request: GraphQLRequest<List<GraphQLResponse<PaginatedResult<ModelWithMetadata<Model>>>>>): List<List<ModelWithMetadata<Model>>> {
-        val response = Amplify.API.query(request).data
-        val responseItemGroups = response.map {
-            it.data?.items?.toList().orEmpty()
-        }
-        val remainRequests = response.mapNotNull {
-            it.data?.requestForNextResult as? AppSyncGraphQLRequest<*>
-        }
-        return if (remainRequests.isEmpty()) {
-            responseItemGroups
-        } else {
-            responseItemGroups + queryAllData(DefaultMergeRequestFactory.merge(remainRequests))
-        }
     }
 
     companion object {
